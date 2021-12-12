0801b85ed2f55e49bfa03de88cb9981c4320d376
Neil E. Pearson <neil@pearson.sydney>
2021-12-13 08:36:54 +1100

    Add docs to utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,7 +1,15 @@
 type obj = Record<string | number | symbol, unknown>;
 
+/**
+ * Type predicate for objects that can respond to any key.
+ */
 export const isObj = (x: unknown): x is obj => x !== null && typeof x === 'object'
 
+/**
+ * Maps an array of key/value pairs to an object, like {@link Object.fromEntries}, but preserves types.
+ * @param array An array of key/value pairs.
+ * @param callback If provided, transforms `array` to the requisite key/value pairs before mapping.
+ */
 export const mapToObj = <K extends keyof never, V, I>(
   array: Readonly<I[]>,
   callback: (o: I, i: number, a: Readonly<I[]>) => [K, V],
@@ -9,5 +17,11 @@ export const mapToObj = <K extends keyof never, V, I>(
 
 export const unique = <T>(value: T, index: keyof T[], self: T[]): boolean => self.indexOf(value) === index
 
+/**
+ * Returns a new array by replacing a single item at a given index.
+ * @param arr The original array, which will not be modified.
+ * @param index The index of the item in `arr` to be replaced.
+ * @param items Items to replace the item at `index`. Typically one item, but zero or multiple items is also fine.
+ */
 export const replaceAtIndex = <T>(arr: T[], index: number, ...items: T[]): T[] =>
   [...arr.slice(0, index), ...items, ...arr.slice(index+1)]
