820e6219b68ef7816a540cd538f0a5eb2aa00fd9
Neil E. Pearson <neil.pearson@bigcommerce.com>
2021-12-13 15:32:44 +1100

    Persist app state to browser location hash for embettered shareabilities

--- src/boot.tsx
+++ src/boot.tsx
@@ -7,13 +7,21 @@ import { SavedState, load, save } from './persistence'
 export const boot = <T extends unknown>(aggregate: Aggregate<T>, view: View<T>) => {
   const container = document.getElementById('app')
   const key = view.name
-  const onChange = (state: SavedState) => save(key, state)
-
+  const onChange = (state: SavedState) => {
+    history.replaceState(null, '', '/#' + encodeURIComponent(JSON.stringify(state)))
+    save(key, state)
+  }
+  let initialState: SavedState
+  try {
+    initialState = JSON.parse(decodeURIComponent(location.hash.slice(1)))
+  } catch(e) {
+    initialState = load(key)
+  }
   render(
     <App
       aggregate={aggregate}
       view={view}
-      initialState={load(key)}
+      initialState={initialState}
       /* eslint-disable-next-line react/jsx-no-bind */
       onChange={onChange}
     />,
