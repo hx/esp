88dc3163e8d19b01de8cf26a15fa3fd314cb59d5
Willem Homan <willem.homan@bigcommerce.com>
2021-12-14 20:16:31 +1100

    Remove debug

--- src/domain/checkout/productLineItem/ItemsView.tsx
+++ src/domain/checkout/productLineItem/ItemsView.tsx
@@ -21,7 +21,6 @@ export const ItemsView: FC<Props> = ({items, format, cart, catalogue}) => {
   }
 
   const saleItems = useMemo(() => items.filter(isSaleItem), [items])
-  console.log(cart.items())
   return (
     <table className="table table-striped table-sm monetary">
       <thead>
