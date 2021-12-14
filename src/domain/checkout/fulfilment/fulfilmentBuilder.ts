263ded25e850b3240790fbc78cc798b0056bf420
Willem Homan <willem.homan@bigcommerce.com>
2021-12-14 14:57:42 +1100

    Prevent items from being shipped more than once

--- src/domain/checkout/fulfilment/fulfilmentBuilder.ts
+++ src/domain/checkout/fulfilment/fulfilmentBuilder.ts
@@ -18,12 +18,24 @@ export const buildFulfilmentLineItems = (store: Store, add: EventClassCreator<St
 
 function addFulfilment(store: Store, add: EventClassCreator<Store>) {
   const cart = store.cart
+  const saleItems = cart.saleItems()
+  const unShippedItems = saleItems.filter(i => !cart.shipments().flatMap(s => s.itemIds).includes(i.id))
   const event = add<FulfilmentEvent>('Fulfilment', 'Ship')
     .handle(({event: {args: {itemIDs, address, amount, method}}, reject}) => {
       if (!/^\s*\d+(\s*,\s*\d+)*\s*$/.test(itemIDs)) {
         return reject('Item IDs should be comma-separated integers')
       }
       const ids = itemIDs.trim().split(/\s*,\s*/).map(Number)
+      const saleItemIds = saleItems.map(s => s.id)
+      const unShippedItemIds = unShippedItems.map(s => s.id)
+      ids.forEach((id) => {
+        if (!saleItemIds.includes(id)) {
+          return reject(`Item ${id} does not exist`)
+        }
+        if (!unShippedItemIds.includes(id)) {
+          return reject(`Item ${id} is already assigned to shipment ` + cart.shipments().find(s => s.itemIds.includes(id))!.id)
+        }
+      })
       return {
         ...store,
         cart: new Cart(cart.currencyCode, [
@@ -42,6 +54,6 @@ function addFulfilment(store: Store, add: EventClassCreator<Store>) {
     Object.entries(SHIPPING_METHODS).map(([id, name ]) => ({displayName: name, value: id}))
   )
   event.addArgument('address', 'Address', '12 Pitt St, Sydney 2001')
-  event.addArgument('itemIDs', 'Item IDs', cart.saleItems().map(i => i.id).join(', '))
+  event.addArgument('itemIDs', 'Item IDs', unShippedItems.map(i => i.id).join(', '))
   event.addArgument('amount', 'Cost', 10.00)
 }
